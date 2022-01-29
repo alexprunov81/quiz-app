91bbb0db59ca5218a410956fc42af2bf141a6d52
Alex Prunov <alexprunov81@gmail.com>
2022-01-29 21:55:21 +0300

    right answer

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -8,17 +8,46 @@ const Quiz = props => {
         {
             question: 'Какого цвета небо?',
             rightAnswerId: 2,
+            id: 1,
             answers: [
                 {text: 'Черный', id: 1},
                 {text: 'Синий', id: 2},
                 {text: 'Красный', id: 3},
                 {text: 'Зелёный', id: 4}
             ]
-        }
+        },
+        {
+            question: 'В каком году был основан Санкт-Петербург?',
+            rightAnswerId: 3,
+            id: 2,
+            answers: [
+                {text: '1700', id: 1},
+                {text: '1701', id: 2},
+                {text: '1703', id: 3},
+                {text: '1803', id: 4}
+            ]
+        },
     ])
+    const [activeQuestion, setActiveQuestion] = useState(0)
 
     const onAnswerClickHandler = (answerId) => {
-        console.log('Answer Id ', answerId)
+        const question = quiz[activeQuestion]
+        if(question.rightAnswerId === answerId) {
+            const timeOut = window.setTimeout(() => {
+                if (isQuizFinished()) {
+                    console.log('Finished')
+                } else {
+                    setActiveQuestion(activeQuestion + 1)
+                }
+                window.clearTimeout(timeOut)
+            }, 200)
+        } else {
+
+        }
+    }
+
+    const isQuizFinished = () => {
+        return activeQuestion + 1 === quiz.length
     }
 
     return (
@@ -26,9 +55,11 @@ const Quiz = props => {
             <div className={classes.QuizWrapper}>
                 <h1>Ответьте на все вопросы </h1>
                 <ActiveQuiz
-                    answers={quiz[0].answers}
-                    question={quiz[0].question}
+                    answers={quiz[activeQuestion].answers}
+                    question={quiz[activeQuestion].question}
                     onAnswerClick={onAnswerClickHandler}
+                    quizLength={quiz.length}
+                    answerNumber={activeQuestion + 1}
                 />
             </div>
         </div>
